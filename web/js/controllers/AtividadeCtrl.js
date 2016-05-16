c095c4a46d2f3687d172a0dfdec1dd3548d5a165
Vitor Ferrari <vitorandreazza@hotmail.com>
2016-05-16 02:42:30 -0300

    Gerar Pdf

--- web/js/controllers/AtividadeCtrl.js
+++ web/js/controllers/AtividadeCtrl.js
@@ -6,11 +6,12 @@ app.controller("atividadesCtrl", function ($scope, atividadeAPI, atividades, $ro
     $scope.excluiAtividade = function (id) {
         if (confirm("Confirma a exclusão da Atividade?")) {
             atividadeAPI.deleteAtividade(id)
-            .success(function () {
-                $route.reload();
-            });  
+                    .success(function () {
+                        $route.reload();
+                    });
         }
     };
+
 });
 
 app.controller("novaAtividadeCtrl", function ($scope, atividadeAPI, $location) {
@@ -26,11 +27,57 @@ app.controller("novaAtividadeCtrl", function ($scope, atividadeAPI, $location) {
 
 app.controller("atividadeCtrl", function ($scope, atividade, atividadeAPI, $location) {
     $scope.atividade = atividade.data;
-         
+
     $scope.alterarAtividade = function (atividade) {
         atividadeAPI.setAtividade(atividade)
                 .success(function () {
                     $location.path("/atividades");
                 });
+    };
+
+    $scope.gerarPdf = function () {
+        var doc = new jsPDF();
+        var x = 20, y = 0;
+        //doc.setFont("courier");
+        
+        doc.setFontSize(20);
+        //doc.setFontType("bold");
+        y += 20;
+        doc.text(x+65, y, 'Atividade');
+        
+        doc.setFontSize(12);
+        doc.setFontType("bold");
+        y += 20;
+        doc.text(x, y, 'Tipo:');
+        
+        doc.setFontSize(11);
+        doc.setFontType("normal");
+        y += 5;
+        doc.text(x, y, $scope.atividade.tipo);
+        
+        if ($scope.atividade.tipo === "Moções") {
+            doc.setFontSize(12);
+            doc.setFontType("bold");
+            y += 10;
+            doc.text(x, y, 'Tipo da moção:');
+
+            doc.setFontSize(11);
+            doc.setFontType("normal");
+            y += 5;
+            doc.text(x, y, $scope.atividade.tipoMocao);
+        }
+        
+        doc.setFontSize(12);
+        doc.setFontType("bold");
+        y += 10;
+        doc.text(x, y, 'Ementa:');
+        
+        doc.setFontSize(11);
+        doc.setFontType("normal");
+        y += 5;
+        doc.text(x, y, $scope.atividade.ementa);
+
+        
+        doc.save('PDF.pdf');
     };
 });
