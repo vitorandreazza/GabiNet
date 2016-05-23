3c5a7359eb41b38cd281673547223904656fe0da
Aluno <Aluno@LAB05-AL12>
2016-05-23 16:27:44 -0300

    grafico certo

--- web/js/controllers/GraficoCtrl.js
+++ web/js/controllers/GraficoCtrl.js
@@ -5,18 +5,29 @@ app.controller("graficosCtrl", function ($scope, atividadeAPI) {
         dataString(datas);
         console.log(datas);
         atividadeAPI.getGrafico(datas)
-            .success(function (data) {
-                $scope.grafico = data;
-                $scope.labels = [];
-                $scope.data = [[],[]];
-                $scope.series = ['Aprovado', 'Reprovado'];
-                delete $scope.datas;
-                $scope.graficoForm.$setPristine();
-                console.log($scope.grafico);
-                for (var i = 0; i < $scope.grafico.length; i++) {
-                    $scope.labels[i] = [$scope.grafico[i]['0']];
-                    $scope.data[0][i] = [$scope.grafico[i]['1']];
-                    $scope.data[1][i] = [$scope.grafico[i]['1']];
+            .success(function (dados) {
+//                $scope.labels = [];
+                $scope.data = {
+                    labels: [],
+                    datasets: [{
+                        label: "Quantidade",
+                        backgroundColor: "rgba(255,99,132,0.2)",
+                        borderColor: "rgba(255,99,132,1)",
+                        borderWidth: 1,
+                        hoverBackgroundColor: "rgba(255,99,132,0.4)",
+                        hoverBorderColor: "rgba(255,99,132,1)",
+                        data: []
+                    }]
+                };
+//                $scope.series = ['Aprovado', 'Reprovado'];
+//                delete $scope.datas;
+//                $scope.graficoForm.$setPristine();
+//                console.log(dados);
+                console.log(dados[1][1]);
+                for (var i = 0; i < dados.length; i++) {
+                    $scope.data.labels.push(dados[i][0]);
+                    $scope.data.datasets[0].data.push(dados[i][1]);
+//                    $scope.data[1][i] = [$scope.grafico[i]['1']];
                 }
             }).error(function (data) {
                 console.log(data);
